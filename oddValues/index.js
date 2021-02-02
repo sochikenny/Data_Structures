8085c4adb8d90e194f105d559f885c6948ec4c25
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-02-01 20:36:40 -0500

    another one

--- oddValues/index.js
+++ oddValues/index.js
@@ -11,3 +11,19 @@ function odd(arr){
 
 console.log(odd([1,2,3,4,5]))
 console.log(odd([]))
+
+//Recursive Solution
+function oddValues(arr){
+    let newArr = []
+    if(arr.length === 0){
+        return newArr
+    }
+    if(arr[0]%2 !== 0){
+        newArr.push(arr[0])
+    }
+    newArr = newArr.concat(oddValues(arr.slice(1)))
+    return newArr
+}
+
+console.log(oddValues([1,2,3,4,5]))
+
