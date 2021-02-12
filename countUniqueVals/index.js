4165cf2fa4c38031e4d8c97ab77df91c5103248e
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-02-11 22:12:20 -0500

    another one

---
+++ countUniqueVals/index.js
@@ -0,0 +1,18 @@
+//Implement a function called countUniqueValues which accepts a sorted array,
+//and counts the unique values in the array. There can be negative numbers in
+//the array, but it will always be sorted
+
+function countUniqueValues(arr){
+    if (arr.length === 0) return 0
+    let i = 0
+    for(let j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++
+            arr[i] = arr[j]
+        }
+    }
+    return i+1
+}
+
+console.log(countUniqueValues([1,1,2,3,4,4,5,6,7,7,8,9]))
+console.log(countUniqueValues([1,1,2,3,3,3,3,3,3,3,3,3,3]))
