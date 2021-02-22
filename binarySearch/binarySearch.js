cd28e178475c2e2fafb0054f7a3fb50819f7d010
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-02-21 20:41:23 -0500

    another one

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,27 +1,46 @@
 //A function that takes in an array and target and outputs its index in the array
 
-function binarySearch(arr, target){
-    let leftIdx = 0;
-    let rightIdx = arr.length - 1
-    let midVal = Math.floor((leftIdx+rightIdx)/2)
-//Dealing with all the initial indices
+// function binarySearch(arr, target){
+//     let leftIdx = 0;
+//     let rightIdx = arr.length - 1
+//     let midVal = Math.floor((leftIdx+rightIdx)/2)
+// //Dealing with all the initial indices
 
-    while(leftIdx < rightIdx){
-        if (target < arr[midVal]){
-            rightIdx = midVal - 1
+//     while(leftIdx < rightIdx){
+//         if (target < arr[midVal]){
+//             rightIdx = midVal - 1
+//         }else{
+//             leftIdx = midVal + 1
+//         }
+//             midVal = Math.floor((leftIdx+rightIdx)/2)
+//     }
+//     if(target === arr[midVal]){
+//         return midVal
+//     }
+//     return -1
+// }
+
+// console.log(binarySearch([1,2,3,4,5], 3))
+//Time Complexity: O(log(n)); Space Complexity: O(1)
+
+function Bsearch(arr, target){
+    let left = 0
+    let right = arr.length - 1
+
+    while(left <= right){
+        let midVal = Math.floor((left+right)/2)
+        if(target < arr[midVal]){
+            right = midVal - 1
+        }else if(target > arr[midVal]){
+            left = midVal + 1
         }else{
-            leftIdx = midVal + 1
+            return midVal
         }
-            midVal = Math.floor((leftIdx+rightIdx)/2)
-    }
-    if(target === arr[midVal]){
-        return midVal
     }
     return -1
 }
 
-console.log(binarySearch([1,2,3,4,5], 3))
-//Time Complexity: O(log(n)); Space Complexity: O(1)
+console.log(Bsearch([1,2,3,4,5], 3))
 
 //-------------------------- Linear Search ---------------------------//
 function linear(arr, target){
