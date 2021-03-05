bb690c01d58efdf0b9164347a06731db07b62f1e
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-03-04 19:10:12 -0500

    another one

---
+++ groupAnagrams /concept.js
@@ -0,0 +1,7 @@
+// conceptual overview
+// 1) initialize object to store(cache)
+// 2) loop thru array
+// 3) use prime numbers to find unique keys of characters 
+// 4) if key is in obj then push value of str in the object
+// 5) else create an array with the value of string in it
+// 6) return object values of object
