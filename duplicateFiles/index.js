8db7bbe14f5ed8200d4090f5817c4892305ec34f
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-03-11 10:18:56 -0500

    another one

---
+++ duplicateFiles/index.js
@@ -0,0 +1,48 @@
+// Input:
+// ["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"]
+
+// Output:  
+// [["root/a/2.txt","root/c/d/4.txt","root/4.txt"],["root/a/1.txt","root/c/3.txt"]]
+
+
+function findDuplicate(paths){
+    let group = {} //initialize a hash table(object in JS)
+    for(let i = 0; i < paths.length; i++){ //interate thru the paths(input)
+        let path = paths[i].split(' ') //split each path by space to see easily see info
+        for(let j = 1; j < path.length; j){  //iterate each path
+            let temp = path[j].split('(') //for each path, I will split by parentheses
+            if(group[temp[1]]=== null){
+                group[temp[1]] = []
+            }
+            group[temp[1]].push(path[0]+'/'+temp[0])
+        }
+    }
+    return [...Object.values(group)].filter((val)=> val.length > 1)
+}
+
+console.log(findDuplicate(["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"]))
+
+
+
+
+// function findDuplicate(paths){
+//     let group = {} //initialize a hash table(object in JS)
+//     for(let i = 0; i < paths.length; i++){ //interate thru the paths(input)
+//         let path = paths[i].split(' ') //split each path by space to see easily see info
+//         for(let j = 1; j < path.length; j){  //iterate each path
+//             let temp = path[j].split('(') //for each path, I will split by parentheses
+//             if(group[temp[1]]=== null){
+//                 group[temp[1]] = []
+//             }
+//             group[temp[1]].push(path[0]+'/'+temp[0])
+//         }
+//     }
+//     let duplicateFilePaths = [];
+//     Object.values(group).forEach(key => {
+//         if (group[key].length > 1) {
+//             duplicateFilePaths.push(group[key]);
+//         }
+//     });
+//     return duplicateFilePaths;
+
+// }
--- palindrome/index.js
+++ palindrome/index.js
@@ -11,5 +11,18 @@ function palindrome(str){
     return true
 }
 
+// function palindrome(str){
+//     let left = 0;
+//     let right = str.length - 1;
+//     while(left < right){
+//         if(str[left]===str[right]){
+//             return true
+//         } 
+//             left ++
+//             right --
+//     }
+//     return false
+// }
+
 console.log(palindrome('tacocat'))
 console.log(palindrome('tacocatk'))
