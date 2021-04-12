deccf3223b1c1e8e320a4049468f10b0fbd1059a
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-04-12 12:42:31 -0400

    another one

--- ArrayOfProducts/index.js
+++ ArrayOfProducts/index.js
@@ -12,4 +12,54 @@ function arrayOfProducts(array) {
     return result
 }
 
+
+//Optimal Solution
+function arrayOfProducts(array) {
+    
+      let products = new Array(array.length).fill(1)
+      let leftProducts = new Array(array.length).fill(1)
+      let rightProducts = new Array(array.length).fill(1)
+      
+      //constructing our left product array
+      let leftRunProduct = 1
+      for (let i = 0; i < array.length; i++){
+          leftProducts[i] = leftRunProduct
+          leftRunProduct *= array[i]
+      }
+      
+      //constructing our right product array
+      let rightRunProduct = 1
+      for(let i = array.length - 1; i > -1; i--){
+          rightProducts[i] = rightRunProduct
+          rightRunProduct *= array[i]
+      }
+      
+      for (let i = 0; i < array.length; i++){
+          products[i] = leftProducts[i] * rightProducts[i]
+      }
+      
+      return products 
+  }
+
+//More Optimal
+  function arrayOfProducts(array) {
+    
+      const products = new Array(array.length).fill(1)
+      
+      let leftRunProduct = 1
+      for(let i=0; i < array.length; i++){
+          products[i] = leftRunProduct
+          leftRunProduct *= array[i]
+      }
+      
+      let rightRunProduct = 1
+      for(let i=array.length - 1; i > -1; i--){
+          products[i] *= rightRunProduct
+          rightRunProduct *= array[i]
+      }
+      
+      return products
+  }
+  
+
 console.log(arrayOfProducts([5,1,4,2]))
