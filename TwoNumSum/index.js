8c5dbea2e2c0ef7b8c607d121342cc95eba7b1a8
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-05-01 09:48:29 -0400

    another one

--- TwoNumSum/index.js
+++ TwoNumSum/index.js
@@ -2,7 +2,7 @@ function twoNumSum(arr, target){
     let obj = {} //initialize obj
 
     for(let i = 0; i < arr.length; i++){ //loop thru array
-        let match = target - arr[i] //create a match(involing a little math)
+        let match = target - arr[i] //create a variable called match(involing a little math)
         if(match in obj){   //if match is in object then return it with the arr value
             return [match, arr[i]]
         }else{              //if not, then construct the initialized object with the arr val
