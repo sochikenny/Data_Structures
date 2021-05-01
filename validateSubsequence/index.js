7d5a831645817be4a95ff056ff21b3588269d2c9
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-05-01 10:28:20 -0400

    another one

--- validateSubsequence/index.js
+++ validateSubsequence/index.js
@@ -1,14 +1,14 @@
 function validateSubsequence(array, sequence){
-    let arrIdx = 0
+    //initialize both array and sequence indexes at 0
+    let arrIdx = 0  
     let seqIdx = 0
-
-    while(arrIdx < array.length && seqIdx < sequence.length){
-        if(array[arrIdx] === sequence[seqIdx])
+    while(arrIdx < array.length && seqIdx < sequence.length){ //while loop on both
+     if(array[arrIdx] === sequence[seqIdx])//if values in both arrays equal, increment both array
         seqIdx ++
         arrIdx ++
     }
 
-    return seqIdx === sequence.length
+    return seqIdx === sequence.length  //afterwards set seqIdx to its length
 }
 
 //Time --> O(N) where N is the size of array
