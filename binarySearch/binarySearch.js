4e228a85802e3a9b9b9e66fe6a5d7064a423b030
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-08-01 13:42:03 -0400

    minor tweak

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,25 +1,5 @@
 //A function that takes in an array and target and outputs its index in the array
 
-// function binarySearch(arr, target){
-//     let leftIdx = 0;
-//     let rightIdx = arr.length - 1
-//     let midVal = Math.floor((leftIdx+rightIdx)/2)
-// //Dealing with all the initial indices
-
-//     while(leftIdx < rightIdx){
-//         if (target < arr[midVal]){
-//             rightIdx = midVal - 1
-//         }else{
-//             leftIdx = midVal + 1
-//         }
-//             midVal = Math.floor((leftIdx+rightIdx)/2)
-//     }
-//     if(target === arr[midVal]){
-//         return midVal
-//     }
-//     return -1
-// }
-
 // console.log(binarySearch([1,2,3,4,5], 3))
 //Time Complexity: O(log(n)); Space Complexity: O(1)
 
@@ -29,7 +9,7 @@ function Bsearch(arr, target){
     while(left <= right){ 
         let midVal = Math.floor((left+right)/2)   
         if(target < arr[midVal]){
-            right = midVal - 1
+            right = midVal - 1 
         }else if(target > arr[midVal]){
             left = midVal + 1
         }else{
@@ -39,7 +19,7 @@ function Bsearch(arr, target){
     return -1
 }
 
-console.log(Bsearch([1,2,3,4,5], 1))
+console.log(Bsearch([1,2,3,4,5], 4))
 
 //-------------------------- Linear Search ---------------------------//
 function linear(arr, target){
